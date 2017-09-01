53320694457338f7572bc1bdf3b8561e845d1334
MisterRiggs <matthew.riggle33@gmail.com>
2017-09-01 18:14:01 -0400

    Commit

---
+++ src/phpWord.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+$data = $_POST['data'];
+$id = $_POST['id'];
+list($type, $data) = explode(';', $data);
+list(, $data)      = explode(',', $data);
+$data = base64_decode($data);
+
+file_put_contents('images/'.$id.'.png', $data);
+
+
+
+
+
+
+
+?>
